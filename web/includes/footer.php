404066b355cb81b0b52dc6af4a53a9c53d20cd1e
opalaciosanchez <oscar.palacios@outlook.com>
2015-07-17 17:13:51 +0200

    Pie y encabezado finalizados
    Sólo falta cambiar los contenidos si es preciso

--- web/includes/footer.php
+++ web/includes/footer.php
@@ -1,13 +1,39 @@
 	<!-- nos aseguramos de que el footer se comporta como una navegación fija en la parte inferior -->
-	<footer class="container-fluid navbar-fixed-bottom row">
-		<div class="pie col-xs-12 col-md-4">
-			<h3>PLATAFORMA</h3>
+	<footer class="container-fluid navbar-fixed-bottom">
+		<div class="row">
+			<div class="pie col-xs-12 col-md-4">
+				<ul>
+					<li><h3>PLATAFORMA</h3></li>
+					<li><a href="#">Servicios</a></li>
+					<li><a href="#">Enfermedades</a></li>
+					<li><a href="#">Consejos de salud</a></li>
+				</ul>
+			</div>
+			<div class="pie col-xs-12 col-md-4">
+				<ul>
+					<li><h3>OPEN DATA</h3></li>
+					<li><a href="#">Open Data</a></li>
+					<li><a href="#">Socrata</a></li>
+					<li><a href="#">Otro Open Data</a></li>
+				</ul>
+			</div>
+			<div class="pie col-xs-12 col-md-4">
+				<ul>
+					<li><h3>OTROS PROYECTOS</h3></li>
+					<li><a href="#">Salud</a></li>
+					<li><a href="#">Información</a></li>
+					<li><a href="#">Gobierno de Aragón Salud</a></li>
+					<li><a href="#">Otros datos</a></li>
+				</ul>
+			</div>
 		</div>
-		<div class="pie col-xs-12 col-md-4">
-			<h3>OPEN DATA</h3>
-		</div>
-		<div class="pie col-xs-12 col-md-4">
-			<h3>OTROS PROYECTOS</h3>
+		<div class="contacto row">
+			<div class="col-xs-6">
+				
+			</div>
+			<div class="col-xs-6">
+				
+			</div>
 		</div>
 	</footer>
 	<!-- Incluimos jQuery y bootstrap.js, que precisa del anterior -->
